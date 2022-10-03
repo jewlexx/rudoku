e14dc94909289e55465f7e046af06b4f9feea65d
Juliette Cordor <grassojames5@gmail.com>
2022-10-03 16:19:13 +1100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,3 @@ pub mod seed;
 pub fn gen_seed() -> Seed {
     Seed::default()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_gen_seed() {
-        let seed = gen_seed();
-
-        println!("{:?}", seed);
-    }
-}
