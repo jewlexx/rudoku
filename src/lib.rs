e084076b97f9f318dd8dbb7d0903ec56589de35a
Juliette Cordor <grassojames5@gmail.com>
2022-10-03 16:07:11 +1100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,14 @@ pub type Seed = <ChaCha8Rng as SeedableRng>::Seed;
 pub fn gen_seed() -> Seed {
     thread_rng().gen()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gen_seed() {
+        let seed = gen_seed();
+        assert_eq!(seed.len(), 32);
+    }
+}
